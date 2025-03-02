1d9813a520226ab8a7ea4311eef76e973761eb98
Adrian Lee <alee.lcagency@gmail.com>
2025-03-02 23:52:28 +0900

    feat: job router

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 import healthRouter from './routers/health'
+import jobRouter from './routers/job'
 
 const app = new Hono()
 
@@ -9,5 +10,6 @@ app.use('*', logger())
 
 // Mount routers
 app.route('/', healthRouter)
+app.route('/', jobRouter)
 
 export default app
