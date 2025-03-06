7d419ad4f4f9715f4b248d1f4773bc34445efa56
Adrian Lee <alee.lcagency@gmail.com>
2025-03-06 23:57:42 +0900

    feat: error handler

--- src/env.ts
+++ src/env.ts
@@ -3,11 +3,21 @@ type Env = {
   REDIS_PORT: number
   DATABASE_URL: string
   PORT: number
+  NODE_ENV: string
+  S3_URL: string
+  S3_REGION: string
+  S3_ACCEESS_ID: string
+  S3_SECRET_KEY: string
 }
 
 export const env: Env = {
   REDIS_HOST: process.env.REDIS_HOST ?? 'localhost',
   REDIS_PORT: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379,
   DATABASE_URL: process.env.DATABASE_URL ?? '',
-  PORT: process.env.PORT ? parseInt(process.env.PORT) : 3000
+  PORT: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+  NODE_ENV: process.env.NODE_ENV ?? 'development',
+  S3_URL: process.env.S3_URL ?? 'http://localhost:9000',
+  S3_REGION: process.env.S3_REGION ?? 'us-east-1',
+  S3_ACCEESS_ID: process.env.S3_ACCEESS_ID ?? 'minioadmin',
+  S3_SECRET_KEY: process.env.S3_SECRET_KEY ?? 'minioadmin'
 }
---
+++ src/util/s3.ts
@@ -0,0 +1,107 @@
+import {
+  DeleteObjectCommand,
+  GetObjectCommand,
+  PutObjectCommand,
+  S3Client
+} from '@aws-sdk/client-s3'
+import crypto from 'crypto'
+import { env } from '../env'
+
+const s3Client = new S3Client({
+  endpoint: env.S3_URL,
+  region: env.S3_REGION,
+  credentials: {
+    accessKeyId: env.S3_ACCEESS_ID,
+    secretAccessKey: env.S3_SECRET_KEY
+  },
+  forcePathStyle: env.NODE_ENV === 'development' // true value Required for MinIO
+})
+
+export const uploadToS3 = async (
+  bucketName: string,
+  key: string,
+  data: Buffer | string
+) => {
+  const command = new PutObjectCommand({
+    Bucket: bucketName,
+    Key: key,
+    Body: data,
+    ContentType: 'application/json'
+  })
+
+  try {
+    const response = await s3Client.send(command)
+    return {
+      success: true,
+      response
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      success: false,
+      error
+    }
+  }
+}
+
+export const downloadAndDecryptFromS3 = async (
+  bucketName: string,
+  key: string,
+  privateKey: string
+) => {
+  const command = new GetObjectCommand({
+    Bucket: bucketName,
+    Key: key
+  })
+
+  try {
+    const response = await s3Client.send(command)
+    const encryptedBuffer = await response.Body?.transformToByteArray()
+
+    if (!encryptedBuffer)
+      return {
+        success: false,
+        error: 'No data found'
+      }
+
+    const decryptedData = crypto.privateDecrypt(
+      privateKey,
+      Buffer.from(encryptedBuffer)
+    )
+
+    const jsonData = JSON.parse(decryptedData.toString()) as Record<
+      string,
+      unknown
+    >
+
+    return {
+      success: true,
+      data: jsonData
+    }
+  } catch (error) {
+    return {
+      success: false,
+      error: error instanceof Error ? error.message : String(error)
+    }
+  }
+}
+
+export const deleteS3Object = async (bucketName: string, key: string) => {
+  const command = new DeleteObjectCommand({
+    Bucket: bucketName,
+    Key: key
+  })
+
+  try {
+    const response = await s3Client.send(command)
+    return {
+      success: true,
+      response
+    }
+  } catch (error) {
+    return {
+      success: false,
+      error: error instanceof Error ? error.message : String(error)
+    }
+  }
+}
