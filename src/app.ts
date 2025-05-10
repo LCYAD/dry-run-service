50689c4901f6fa04c9689a16dba26885785c5684
Adrian Lee <alee.lcagency@gmail.com>
2025-05-10 16:55:01 +0900

    feat: add bull board

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,27 @@
+import { createBullBoard } from '@bull-board/api'
+import { serveStatic } from '@hono/node-server/serve-static'
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
+
+// NOTE: switch commented line below for DENO only (since the package is already converted to ESM)
+// import { BullMQAdapter } from '@bull-board/api/bullMQAdapter.js'
+import { BullMQAdapter } from '@bull-board/api/bullMQAdapter'
+
+import { HonoAdapter } from '@bull-board/hono'
+import { testStringEqualQueue } from './queues/testStringEqual'
 import failedJobRouter from './routers/failedJob'
 import healthRouter from './routers/health'
+
 import jobRouter from './routers/job'
+
+const serverAdapter = new HonoAdapter(serveStatic)
+
+createBullBoard({
+  queues: [new BullMQAdapter(testStringEqualQueue)],
+  serverAdapter
+})
+
+serverAdapter.setBasePath('/bull-board')
 
 const app = new Hono()
 
@@ -13,5 +32,8 @@ app.use('*', logger())
 app.route('/', healthRouter)
 app.route('/', jobRouter)
 app.route('/', failedJobRouter)
+
+// @ts-expect-error it works but types are not correct
+app.route('/bull-board', serverAdapter.registerPlugin())
 
 export default app
